1674fcb8dd4b884ef3e7a06b6dde4e85af73275c
zxeni1 <zacxfitness@gmail.com>
2024-11-16 09:52:55 +1000

    finalise classes

--- jsclasses/app.js
+++ jsclasses/app.js
@@ -4,10 +4,47 @@ class Dog {
         this.breed = breed;
         this.age = age;
         this.color = color;
+        Dog.#numberOfDogs++;
+    }
+    static #numberOfDogs = 0;
+
+    static resetDogCount(){
+        Dog.#numberOfDogs = 0;
+    }
+
+    // static set numberOfDogs(newValue){
+    //     return Dog.#numberOfDogs++;
+    // }
+
+    bark() {
+        // console.log(`Woof! Woof! I am ${this.name}.`);
+        throw new BarkError(this.name, this.breed);
     }
 }
 
-let brockie = new Dog('Brockie', 'German Shepard', '3', 'Chocolate');
+let brockie = new Dog('Brockie', 'German Shepherd', '3', 'Brown');
 
-console.log(brockie);
+// console.log(brockie.bark());
+
+// create unique BarkError class by inheriting from Error class
+class BarkError extends Error {
+    constructor(dogName, dogBreed){
+        // super() is called to use the Error class's properties/methods
+        super(`${dogName} of breed ${dogBreed} failed to bark.`);
+    }
+}
+
+// let newError = new Error()
+try {
+    brockie.bark();
+} catch (error) {
+    // throw new BarkError(brockie.name, brockie.breed);
+    if (BarkError){
+        console.log("Bark error occured.");
+    }
+}
+
+Dog.numberOfDogs = 10;
+console.log(Dog.numberOfDogs);
+
 
