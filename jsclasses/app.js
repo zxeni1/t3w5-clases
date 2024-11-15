83079000ed2ddeb9d5b290a2fc21f569dbf465e6
zxeni1 <zacxfitness@gmail.com>
2024-11-16 09:02:45 +1000

    feat: example dog class

---
+++ jsclasses/app.js
@@ -0,0 +1,13 @@
+class Dog {
+    constructor(name, breed, age, color){
+        this.name = name;
+        this.breed = breed;
+        this.age = age;
+        this.color = color;
+    }
+}
+
+let brockie = new Dog('Brockie', 'German Shepard', '3', 'Chocolate');
+
+console.log(brockie);
+
