78e2fc458cb908c6bde613c730927efc96051f08
zxeni1 <zacxfitness@gmail.com>
2024-11-16 11:31:45 +1000

    feat: use API to get pokemon data

--- pokemon-class-demo/src/PokemonFetcher.jsx
+++ pokemon-class-demo/src/PokemonFetcher.jsx
@@ -7,11 +7,22 @@ export default class PokemonFetcher extends Component {
     constructor(props){
         super(props);
 
+
         this.state = {
-            pokemonList: ['pikachu', 'ditto', 'mew', 'charmander']
+            pokemonList: []
         }
     }
 
+    async componentDidMount(){
+        // Generate a random Pokemon ID number
+        let randomNumber = Math.ceil(Math.random() * 1025);
+        // Pass the random pokemon number to fetch
+        let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`)
+        let data = await response.json();
+        // set fetch response data to state
+        this.setState({pokemonList: [data]});
+        console.log("PokemonFetcher first load on the page");
+    }
 
     render() {
         return (
@@ -19,11 +30,11 @@ export default class PokemonFetcher extends Component {
                 <h1>Pokemon Data</h1>
                 {
                     this.state.pokemonList.map(pokemon => {
-                        return <PokemonCard name={pokemon} />
+                        return <PokemonCard name={pokemon.name} />
                     })
                 }
                 <button onClick={() => {
-                    this.setState({pokemonList: []})
+                    this.setState({pokemonList: []});
                 }}>Empty the state</button>
             </div>
 
